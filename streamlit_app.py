7f7780131f62f2d51f94985c899fdab4a694813e
veeruhai <veeruhai@gmail.com>
2022-09-01 23:07:46 -0400

    Update streamlit_app.py
    sdfsdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("my premts new healthy Diner")
+streamlit.header("Breakfast menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale")
+streamlit.text("Hard-Biled Free-range Egg")
+
